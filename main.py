e9a8adfa05fa282e5e08626ed70043bb9de7ade9
YogPanjarale <yog.panjarale@gmail.com>
2021-05-03 19:44:46 +0530

    basic add get

--- main.py
+++ main.py
@@ -1,11 +1,52 @@
-from flask import Flask
-print(__name__)
+from flask import Flask, jsonify, request
+
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "Hello World"
+tasks = [
+    {
+        'id': 1,
+        'title': "Learn Python",
+        'description': "look for tutorials On Internet",
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': "Make Api",
+        'description': "Make api from flask",
+        'done': False
+    },
+    {
+        'id': 3,
+        'title': "Publish Api",
+        'description': "Research about how to publish flask api on internet",
+        'done': False
+    }
+]
 
 
-if (__name__ =="__main__"):
-    app.run()
+@app.route("/add-data", methods=['POST'])
+def add_data():
+    if (not request.json):
+        return jsonify({
+            "Status": "Error 400",
+            "Message": "Data not Provided"
+        }, 400)
+    task ={
+        'id': tasks[-1]['id'] +1,
+        'title': request.json['title'],
+        'description': request.json.get('description',""),
+        'done': False
+    }
+    tasks.append(task)
+    return jsonify({
+        "Status":"Sucess",
+        "Message":"Task Added Sucessfully"
+    })
+@app.route("/get-data")
+def get_data():
+    return jsonify({
+        "Data":tasks
+    })
+
+if (__name__ == "__main__"):
+    app.run(debug=True)
