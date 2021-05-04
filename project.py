a6475ba5c930bca2869feb8eb2ae132059e569de
YogPanjarale <yog.panjarale@gmail.com>
2021-05-04 11:47:34 +0530

    code formated

--- project.py
+++ project.py
@@ -1,38 +1,38 @@
-from flask import Flask,request,jsonify
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-contacts=[
+contacts = [
     {
-        "Contact":"99876444560",
+        "Contact": "99876444560",
         "Name": "Raju",
-        "done":False,
-        "id":1
+        "done": False,
+        "id": 1
     },
     {
-        "Contact":"9876543222",
+        "Contact": "9876543222",
         "Name": "Rahul",
-        "done":False,
-        "id":2
+        "done": False,
+        "id": 2
     },
 ]
 
-@app.route("/add-data",methods=['POST'])
+
+@app.route("/add-data", methods=['POST'])
 def add_data():
     if not request.json:
         return jsonify({
             "Status": "Error 400",
             "Message": "Data not Provided"
         }, 400)
-    contact ={
-        'id': contacts[-1]['id'] +1,
+    contact = {
+        'id': contacts[-1]['id'] + 1,
         'Name': request.json['Name'],
-        'Contact': request.json.get('Contact',""),
+        'Contact': request.json.get('Contact', ""),
         'done': False
     }
     contacts.append(contact)
     return jsonify({
-        "Status":"Sucess",
-        "Message":"Task Added Sucessfully"
+        "Status": "Sucess",
+        "Message": "Task Added Sucessfully"
     })
-    pass
